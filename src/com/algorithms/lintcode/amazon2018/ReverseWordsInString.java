012a530850f052e02284f8d003ea29e502e5866c
Ming Li <li.ming116@gmail.com>
2018-08-07 23:55:44 +0100

    Amazon 2018 OA easy

--- src/com/algorithms/lintcode/amazon2018/ReverseWordsInString.java
+++ src/com/algorithms/lintcode/amazon2018/ReverseWordsInString.java
@@ -32,4 +32,24 @@ public class ReverseWordsInString {
             j--;
         }
     }
+    
+    public String reverseWords2(String s) {
+        // write your code here
+        if(s.length() == 0 || s == null){
+            return " ";
+        }
+        String[] array = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = array.length - 1; i >= 0; i--){
+            if(!array[i].equals("")) {
+                if (sb.length() > 0) {
+                    sb.append(" ");
+                }
+                
+                sb.append(array[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
